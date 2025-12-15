6dffd3e4360c92545d60ad21af58d19e46384cb3
GMBY <gmaxby@MacBook-Pro.local>
2025-12-15 17:53:48 +0300

    Add more manufacturer logos (K-T) to SQL update script
    - Add 120+ manufacturer logo updates (K-T range)
    - Handle empty logos for: RWD Life Science, Seramun, Shanghai Korain Biotech Company, Sigma Laboratory Centrifuges, Sisco, Spectrum
    - Update verification query to include all manufacturers

--- prisma/update-manufacturer-logos.sql
+++ prisma/update-manufacturer-logos.sql
@@ -1,6 +1,671 @@
 -- Update manufacturer logos in Supabase
 -- Run this script in Supabase SQL Editor
 
+-- Karl Storz
+UPDATE "Manufacturer" 
+SET logo = '/logos/karl-storz.png' 
+WHERE LOWER(name) = LOWER('Karl Storz');
+
+-- Kementec
+UPDATE "Manufacturer" 
+SET logo = '/logos/kementec.png' 
+WHERE LOWER(name) = LOWER('Kementec');
+
+-- Krishgen
+UPDATE "Manufacturer" 
+SET logo = '/logos/krishgen.png' 
+WHERE LOWER(name) = LOWER('Krishgen');
+
+-- Kyokuto
+UPDATE "Manufacturer" 
+SET logo = '/logos/kyokuto.png' 
+WHERE LOWER(name) = LOWER('Kyokuto');
+
+-- Labor Diagnostika
+UPDATE "Manufacturer" 
+SET logo = '/logos/labor-diagnostika.png' 
+WHERE LOWER(name) = LOWER('Labor Diagnostika');
+
+-- Larodan
+UPDATE "Manufacturer" 
+SET logo = '/logos/larodan.png' 
+WHERE LOWER(name) = LOWER('Larodan');
+
+-- Leinco Technologies
+UPDATE "Manufacturer" 
+SET logo = '/logos/leinco-technologies.png' 
+WHERE LOWER(name) = LOWER('Leinco Technologies');
+
+-- LGC
+UPDATE "Manufacturer" 
+SET logo = '/logos/lgc.png' 
+WHERE LOWER(name) = LOWER('LGC');
+
+-- Life Technologies Corporation
+UPDATE "Manufacturer" 
+SET logo = '/logos/life-technologies-corporation.png' 
+WHERE LOWER(name) = LOWER('Life Technologies Corporation');
+
+-- LKT
+UPDATE "Manufacturer" 
+SET logo = '/logos/lkt.png' 
+WHERE LOWER(name) = LOWER('LKT');
+
+-- LKT Labs
+UPDATE "Manufacturer" 
+SET logo = '/logos/lkt-labs.png' 
+WHERE LOWER(name) = LOWER('LKT Labs');
+
+-- LOEWE
+UPDATE "Manufacturer" 
+SET logo = '/logos/loewe.png' 
+WHERE LOWER(name) = LOWER('LOEWE');
+
+-- Logos
+UPDATE "Manufacturer" 
+SET logo = '/logos/logos.png' 
+WHERE LOWER(name) = LOWER('Logos');
+
+-- Lonza
+UPDATE "Manufacturer" 
+SET logo = '/logos/lonza.png' 
+WHERE LOWER(name) = LOWER('Lonza');
+
+-- Lubio
+UPDATE "Manufacturer" 
+SET logo = '/logos/lubio.png' 
+WHERE LOWER(name) = LOWER('Lubio');
+
+-- Lucigen
+UPDATE "Manufacturer" 
+SET logo = '/logos/lucigen.png' 
+WHERE LOWER(name) = LOWER('Lucigen');
+
+-- Ludger
+UPDATE "Manufacturer" 
+SET logo = '/logos/ludger.png' 
+WHERE LOWER(name) = LOWER('Ludger');
+
+-- Mabtech
+UPDATE "Manufacturer" 
+SET logo = '/logos/mabtech.png' 
+WHERE LOWER(name) = LOWER('Mabtech');
+
+-- Macherey-Nagel
+UPDATE "Manufacturer" 
+SET logo = '/logos/macherey-nagel.png' 
+WHERE LOWER(name) = LOWER('Macherey-Nagel');
+
+-- MCE
+UPDATE "Manufacturer" 
+SET logo = '/logos/mce.png' 
+WHERE LOWER(name) = LOWER('MCE');
+
+-- medac
+UPDATE "Manufacturer" 
+SET logo = '/logos/medac.png' 
+WHERE LOWER(name) = LOWER('medac');
+
+-- MedChemExpress
+UPDATE "Manufacturer" 
+SET logo = '/logos/medchemexpress.png' 
+WHERE LOWER(name) = LOWER('MedChemExpress');
+
+-- Megazyme
+UPDATE "Manufacturer" 
+SET logo = '/logos/megazyme.png' 
+WHERE LOWER(name) = LOWER('Megazyme');
+
+-- Mercedes Scientific
+UPDATE "Manufacturer" 
+SET logo = '/logos/mercedes-scientific.png' 
+WHERE LOWER(name) = LOWER('Mercedes Scientific');
+
+-- Merck
+UPDATE "Manufacturer" 
+SET logo = '/logos/merck.png' 
+WHERE LOWER(name) = LOWER('Merck');
+
+-- Metrohm
+UPDATE "Manufacturer" 
+SET logo = '/logos/metrohm.png' 
+WHERE LOWER(name) = LOWER('Metrohm');
+
+-- METTLER TOLEDO
+UPDATE "Manufacturer" 
+SET logo = '/logos/mettler-toledo.png' 
+WHERE LOWER(name) = LOWER('METTLER TOLEDO');
+
+-- Mikromol
+UPDATE "Manufacturer" 
+SET logo = '/logos/mikromol.png' 
+WHERE LOWER(name) = LOWER('Mikromol');
+
+-- Millipore
+UPDATE "Manufacturer" 
+SET logo = '/logos/millipore.png' 
+WHERE LOWER(name) = LOWER('Millipore');
+
+-- Minerva
+UPDATE "Manufacturer" 
+SET logo = '/logos/minerva.png' 
+WHERE LOWER(name) = LOWER('Minerva');
+
+-- MoBiTec
+UPDATE "Manufacturer" 
+SET logo = '/logos/mobitec.png' 
+WHERE LOWER(name) = LOWER('MoBiTec');
+
+-- Molecular Probes
+UPDATE "Manufacturer" 
+SET logo = '/logos/molecular-probes.png' 
+WHERE LOWER(name) = LOWER('Molecular Probes');
+
+-- Monosan
+UPDATE "Manufacturer" 
+SET logo = '/logos/monosan.png' 
+WHERE LOWER(name) = LOWER('Monosan');
+
+-- Morphisto
+UPDATE "Manufacturer" 
+SET logo = '/logos/morphisto.png' 
+WHERE LOWER(name) = LOWER('Morphisto');
+
+-- MP Biomedicals
+UPDATE "Manufacturer" 
+SET logo = '/logos/mp-biomedicals.png' 
+WHERE LOWER(name) = LOWER('MP Biomedicals');
+
+-- MRC holland
+UPDATE "Manufacturer" 
+SET logo = '/logos/mrc-holland.png' 
+WHERE LOWER(name) = LOWER('MRC holland');
+
+-- MyBioSource
+UPDATE "Manufacturer" 
+SET logo = '/logos/mybiosource.png' 
+WHERE LOWER(name) = LOWER('MyBioSource');
+
+-- MZ
+UPDATE "Manufacturer" 
+SET logo = '/logos/mz.png' 
+WHERE LOWER(name) = LOWER('MZ');
+
+-- Nalgene
+UPDATE "Manufacturer" 
+SET logo = '/logos/nalgene.png' 
+WHERE LOWER(name) = LOWER('Nalgene');
+
+-- NEB
+UPDATE "Manufacturer" 
+SET logo = '/logos/neb.png' 
+WHERE LOWER(name) = LOWER('NEB');
+
+-- neoFroxx
+UPDATE "Manufacturer" 
+SET logo = '/logos/neofroxx.png' 
+WHERE LOWER(name) = LOWER('neoFroxx');
+
+-- NeoLab
+UPDATE "Manufacturer" 
+SET logo = '/logos/neolab.png' 
+WHERE LOWER(name) = LOWER('NeoLab');
+
+-- Neubauer
+UPDATE "Manufacturer" 
+SET logo = '/logos/neubauer.png' 
+WHERE LOWER(name) = LOWER('Neubauer');
+
+-- NIBSC
+UPDATE "Manufacturer" 
+SET logo = '/logos/nibsc.png' 
+WHERE LOWER(name) = LOWER('NIBSC');
+
+-- Nimagen
+UPDATE "Manufacturer" 
+SET logo = '/logos/nimagen.png' 
+WHERE LOWER(name) = LOWER('Nimagen');
+
+-- NIPPON
+UPDATE "Manufacturer" 
+SET logo = '/logos/nippon.png' 
+WHERE LOWER(name) = LOWER('NIPPON');
+
+-- Nordic-Mubio
+UPDATE "Manufacturer" 
+SET logo = '/logos/nordic-mubio.png' 
+WHERE LOWER(name) = LOWER('Nordic-Mubio');
+
+-- NovusBio
+UPDATE "Manufacturer" 
+SET logo = '/logos/novusbio.png' 
+WHERE LOWER(name) = LOWER('NovusBio');
+
+-- Nunc
+UPDATE "Manufacturer" 
+SET logo = '/logos/nunc.png' 
+WHERE LOWER(name) = LOWER('Nunc');
+
+-- Omega
+UPDATE "Manufacturer" 
+SET logo = '/logos/omega.png' 
+WHERE LOWER(name) = LOWER('Omega');
+
+-- Orgentec
+UPDATE "Manufacturer" 
+SET logo = '/logos/orgentec.png' 
+WHERE LOWER(name) = LOWER('Orgentec');
+
+-- Ortho
+UPDATE "Manufacturer" 
+SET logo = '/logos/ortho.png' 
+WHERE LOWER(name) = LOWER('Ortho');
+
+-- Oxford Nanopore Technologies
+UPDATE "Manufacturer" 
+SET logo = '/logos/oxford-nanopore-technologies.png' 
+WHERE LOWER(name) = LOWER('Oxford Nanopore Technologies');
+
+-- OZ Biosciences
+UPDATE "Manufacturer" 
+SET logo = '/logos/oz-biosciences.png' 
+WHERE LOWER(name) = LOWER('OZ Biosciences');
+
+-- PanReac
+UPDATE "Manufacturer" 
+SET logo = '/logos/panreac.png' 
+WHERE LOWER(name) = LOWER('PanReac');
+
+-- PeproTech
+UPDATE "Manufacturer" 
+SET logo = '/logos/peprotech.png' 
+WHERE LOWER(name) = LOWER('PeproTech');
+
+-- Perkin Elmer
+UPDATE "Manufacturer" 
+SET logo = '/logos/perkin-elmer.png' 
+WHERE LOWER(name) = LOWER('Perkin Elmer');
+
+-- Phadia
+UPDATE "Manufacturer" 
+SET logo = '/logos/phadia.png' 
+WHERE LOWER(name) = LOWER('Phadia');
+
+-- PhytoTech
+UPDATE "Manufacturer" 
+SET logo = '/logos/phytotech.png' 
+WHERE LOWER(name) = LOWER('PhytoTech');
+
+-- Pierce
+UPDATE "Manufacturer" 
+SET logo = '/logos/pierce.png' 
+WHERE LOWER(name) = LOWER('Pierce');
+
+-- pluriSelect
+UPDATE "Manufacturer" 
+SET logo = '/logos/pluriselect.png' 
+WHERE LOWER(name) = LOWER('pluriSelect');
+
+-- Polysciences
+UPDATE "Manufacturer" 
+SET logo = '/logos/polysciences.png' 
+WHERE LOWER(name) = LOWER('Polysciences');
+
+-- PROGEN
+UPDATE "Manufacturer" 
+SET logo = '/logos/progen.png' 
+WHERE LOWER(name) = LOWER('PROGEN');
+
+-- Promega
+UPDATE "Manufacturer" 
+SET logo = '/logos/promega.png' 
+WHERE LOWER(name) = LOWER('Promega');
+
+-- PromoCell
+UPDATE "Manufacturer" 
+SET logo = '/logos/promocell.png' 
+WHERE LOWER(name) = LOWER('PromoCell');
+
+-- ProSci-Inc
+UPDATE "Manufacturer" 
+SET logo = '/logos/prosci-inc.png' 
+WHERE LOWER(name) = LOWER('ProSci-Inc');
+
+-- prospec
+UPDATE "Manufacturer" 
+SET logo = '/logos/prospec.png' 
+WHERE LOWER(name) = LOWER('prospec');
+
+-- ProteinSimple
+UPDATE "Manufacturer" 
+SET logo = '/logos/proteinsimple.png' 
+WHERE LOWER(name) = LOWER('ProteinSimple');
+
+-- Proteintech
+UPDATE "Manufacturer" 
+SET logo = '/logos/proteintech.png' 
+WHERE LOWER(name) = LOWER('Proteintech');
+
+-- QA bio
+UPDATE "Manufacturer" 
+SET logo = '/logos/qa-bio.png' 
+WHERE LOWER(name) = LOWER('QA bio');
+
+-- QED Bioscience
+UPDATE "Manufacturer" 
+SET logo = '/logos/qed-bioscience.png' 
+WHERE LOWER(name) = LOWER('QED Bioscience');
+
+-- Qiagen
+UPDATE "Manufacturer" 
+SET logo = '/logos/qiagen.png' 
+WHERE LOWER(name) = LOWER('Qiagen');
+
+-- Quidel
+UPDATE "Manufacturer" 
+SET logo = '/logos/quidel.png' 
+WHERE LOWER(name) = LOWER('Quidel');
+
+-- R-Biopharm
+UPDATE "Manufacturer" 
+SET logo = '/logos/r-biopharm.png' 
+WHERE LOWER(name) = LOWER('R-Biopharm');
+
+-- R&D Systems
+UPDATE "Manufacturer" 
+SET logo = '/logos/rd-systems.png' 
+WHERE LOWER(name) = LOWER('R&D Systems');
+
+-- Radiometer
+UPDATE "Manufacturer" 
+SET logo = '/logos/radiometer.png' 
+WHERE LOWER(name) = LOWER('Radiometer');
+
+-- Randox Laboratories Ltd.
+UPDATE "Manufacturer" 
+SET logo = '/logos/randox-laboratories-ltd.png' 
+WHERE LOWER(name) = LOWER('Randox Laboratories Ltd.');
+
+-- RayBiotech
+UPDATE "Manufacturer" 
+SET logo = '/logos/raybiotech.png' 
+WHERE LOWER(name) = LOWER('RayBiotech');
+
+-- RD_Tocris
+UPDATE "Manufacturer" 
+SET logo = '/logos/rd_tocris.png' 
+WHERE LOWER(name) = LOWER('RD_Tocris');
+
+-- Reagecon
+UPDATE "Manufacturer" 
+SET logo = '/logos/reagecon.png' 
+WHERE LOWER(name) = LOWER('Reagecon');
+
+-- Reprocell
+UPDATE "Manufacturer" 
+SET logo = '/logos/reprocell.png' 
+WHERE LOWER(name) = LOWER('Reprocell');
+
+-- Retsch
+UPDATE "Manufacturer" 
+SET logo = '/logos/retsch.png' 
+WHERE LOWER(name) = LOWER('Retsch');
+
+-- Roche
+UPDATE "Manufacturer" 
+SET logo = '/logos/roche.png' 
+WHERE LOWER(name) = LOWER('Roche');
+
+-- Roche Diagnostics
+UPDATE "Manufacturer" 
+SET logo = '/logos/roche-diagnostics.png' 
+WHERE LOWER(name) = LOWER('Roche Diagnostics');
+
+-- Rockland
+UPDATE "Manufacturer" 
+SET logo = '/logos/rockland.png' 
+WHERE LOWER(name) = LOWER('Rockland');
+
+-- RWD Life Science (empty logo - set to NULL)
+UPDATE "Manufacturer" 
+SET logo = NULL 
+WHERE LOWER(name) = LOWER('RWD Life Science');
+
+-- SAB
+UPDATE "Manufacturer" 
+SET logo = '/logos/sab.png' 
+WHERE LOWER(name) = LOWER('SAB');
+
+-- SAFC
+UPDATE "Manufacturer" 
+SET logo = '/logos/safc.png' 
+WHERE LOWER(name) = LOWER('SAFC');
+
+-- Santa Crus
+UPDATE "Manufacturer" 
+SET logo = '/logos/santa-crus.png' 
+WHERE LOWER(name) = LOWER('Santa Crus');
+
+-- Santa Cruz
+UPDATE "Manufacturer" 
+SET logo = '/logos/santa-cruz.png' 
+WHERE LOWER(name) = LOWER('Santa Cruz');
+
+-- Sapphire
+UPDATE "Manufacturer" 
+SET logo = '/logos/sapphire.png' 
+WHERE LOWER(name) = LOWER('Sapphire');
+
+-- Sarstedt
+UPDATE "Manufacturer" 
+SET logo = '/logos/sarstedt.png' 
+WHERE LOWER(name) = LOWER('Sarstedt');
+
+-- Sartorius
+UPDATE "Manufacturer" 
+SET logo = '/logos/sartorius.png' 
+WHERE LOWER(name) = LOWER('Sartorius');
+
+-- SBI
+UPDATE "Manufacturer" 
+SET logo = '/logos/sbi.png' 
+WHERE LOWER(name) = LOWER('SBI');
+
+-- Scharlab
+UPDATE "Manufacturer" 
+SET logo = '/logos/scharlab.png' 
+WHERE LOWER(name) = LOWER('Scharlab');
+
+-- Scharlau
+UPDATE "Manufacturer" 
+SET logo = '/logos/scharlau.png' 
+WHERE LOWER(name) = LOWER('Scharlau');
+
+-- Sciex
+UPDATE "Manufacturer" 
+SET logo = '/logos/sciex.png' 
+WHERE LOWER(name) = LOWER('Sciex');
+
+-- Sebia
+UPDATE "Manufacturer" 
+SET logo = '/logos/sebia.png' 
+WHERE LOWER(name) = LOWER('Sebia');
+
+-- Selleck
+UPDATE "Manufacturer" 
+SET logo = '/logos/selleck.png' 
+WHERE LOWER(name) = LOWER('Selleck');
+
+-- Seracare
+UPDATE "Manufacturer" 
+SET logo = '/logos/seracare.png' 
+WHERE LOWER(name) = LOWER('Seracare');
+
+-- Seramun (empty logo - set to NULL)
+UPDATE "Manufacturer" 
+SET logo = NULL 
+WHERE LOWER(name) = LOWER('Seramun');
+
+-- Serva
+UPDATE "Manufacturer" 
+SET logo = '/logos/serva.png' 
+WHERE LOWER(name) = LOWER('Serva');
+
+-- Shanghai Korain Biotech Company (empty logo - set to NULL)
+UPDATE "Manufacturer" 
+SET logo = NULL 
+WHERE LOWER(name) = LOWER('Shanghai Korain Biotech Company');
+
+-- Shodex
+UPDATE "Manufacturer" 
+SET logo = '/logos/shodex.png' 
+WHERE LOWER(name) = LOWER('Shodex');
+
+-- Siemens
+UPDATE "Manufacturer" 
+SET logo = '/logos/siemens.png' 
+WHERE LOWER(name) = LOWER('Siemens');
+
+-- Siemens Healthineers
+UPDATE "Manufacturer" 
+SET logo = '/logos/siemens-healthineers.png' 
+WHERE LOWER(name) = LOWER('Siemens Healthineers');
+
+-- Siemens Allergy
+UPDATE "Manufacturer" 
+SET logo = '/logos/siemens-allergy.png' 
+WHERE LOWER(name) = LOWER('Siemens Allergy');
+
+-- Siemens Coag
+UPDATE "Manufacturer" 
+SET logo = '/logos/siemens-coag.png' 
+WHERE LOWER(name) = LOWER('Siemens Coag');
+
+-- Sigma-Aldrich
+UPDATE "Manufacturer" 
+SET logo = '/logos/sigma-aldrich.png' 
+WHERE LOWER(name) = LOWER('Sigma-Aldrich');
+
+-- Sigma Laboratory Centrifuges (empty logo - set to NULL)
+UPDATE "Manufacturer" 
+SET logo = NULL 
+WHERE LOWER(name) = LOWER('Sigma Laboratory Centrifuges');
+
+-- SignaChem
+UPDATE "Manufacturer" 
+SET logo = '/logos/signachem.png' 
+WHERE LOWER(name) = LOWER('SignaChem');
+
+-- Simport
+UPDATE "Manufacturer" 
+SET logo = '/logos/simport.png' 
+WHERE LOWER(name) = LOWER('Simport');
+
+-- SinoBiological
+UPDATE "Manufacturer" 
+SET logo = '/logos/sinobiological.png' 
+WHERE LOWER(name) = LOWER('SinoBiological');
+
+-- Sisco (empty logo - set to NULL)
+UPDATE "Manufacturer" 
+SET logo = NULL 
+WHERE LOWER(name) = LOWER('Sisco');
+
+-- siTOOLs Biotech
+UPDATE "Manufacturer" 
+SET logo = '/logos/sitools-biotech.png' 
+WHERE LOWER(name) = LOWER('siTOOLs Biotech');
+
+-- Smith & Nephew
+UPDATE "Manufacturer" 
+SET logo = '/logos/smith-nephew.png' 
+WHERE LOWER(name) = LOWER('Smith & Nephew');
+
+-- Sony Biotechnology
+UPDATE "Manufacturer" 
+SET logo = '/logos/sony-biotechnology.png' 
+WHERE LOWER(name) = LOWER('Sony Biotechnology');
+
+-- SouthernBiotech
+UPDATE "Manufacturer" 
+SET logo = '/logos/southernbiotech.png' 
+WHERE LOWER(name) = LOWER('SouthernBiotech');
+
+-- Spectrum (empty logo - set to NULL)
+UPDATE "Manufacturer" 
+SET logo = NULL 
+WHERE LOWER(name) = LOWER('Spectrum');
+
+-- Spherotech
+UPDATE "Manufacturer" 
+SET logo = '/logos/spherotech.png' 
+WHERE LOWER(name) = LOWER('Spherotech');
+
+-- Stago
+UPDATE "Manufacturer" 
+SET logo = '/logos/stago.png' 
+WHERE LOWER(name) = LOWER('Stago');
+
+-- STEMCELL
+UPDATE "Manufacturer" 
+SET logo = '/logos/stemcell.png' 
+WHERE LOWER(name) = LOWER('STEMCELL');
+
+-- StemCell
+UPDATE "Manufacturer" 
+SET logo = '/logos/stemcell.png' 
+WHERE LOWER(name) = LOWER('StemCell');
+
+-- Streck
+UPDATE "Manufacturer" 
+SET logo = '/logos/streck.png' 
+WHERE LOWER(name) = LOWER('Streck');
+
+-- Stryker
+UPDATE "Manufacturer" 
+SET logo = '/logos/stryker.png' 
+WHERE LOWER(name) = LOWER('Stryker');
+
+-- Supelco
+UPDATE "Manufacturer" 
+SET logo = '/logos/supelco.png' 
+WHERE LOWER(name) = LOWER('Supelco');
+
+-- Synaptic Systems
+UPDATE "Manufacturer" 
+SET logo = '/logos/synaptic-systems.png' 
+WHERE LOWER(name) = LOWER('Synaptic Systems');
+
+-- Sysmex
+UPDATE "Manufacturer" 
+SET logo = '/logos/sysmex.png' 
+WHERE LOWER(name) = LOWER('Sysmex');
+
+-- Sysmex UF
+UPDATE "Manufacturer" 
+SET logo = '/logos/sysmex-uf.png' 
+WHERE LOWER(name) = LOWER('Sysmex UF');
+
+-- System Biosciences
+UPDATE "Manufacturer" 
+SET logo = '/logos/system-biosciences.png' 
+WHERE LOWER(name) = LOWER('System Biosciences');
+
+-- Takara Bio
+UPDATE "Manufacturer" 
+SET logo = '/logos/takara-bio.png' 
+WHERE LOWER(name) = LOWER('Takara Bio');
+
+-- TargetMol
+UPDATE "Manufacturer" 
+SET logo = '/logos/targetmol.png' 
+WHERE LOWER(name) = LOWER('TargetMol');
+
+-- TCI
+UPDATE "Manufacturer" 
+SET logo = '/logos/tci.png' 
+WHERE LOWER(name) = LOWER('TCI');
+
 -- Ted Pella
 UPDATE "Manufacturer" 
 SET logo = '/logos/ted-pella.png' 
@@ -205,9 +870,31 @@ WHERE LOWER(name) = LOWER('Zytomed');
 SELECT name, logo 
 FROM "Manufacturer" 
 WHERE LOWER(name) IN (
-  'ted pella', 'teknova', 'texwipe', 'tfs', 'the native antigen company',
-  'thermo fisher scientific', 'thermo', 'tocris bioscience', 'toku-e',
-  'toronto research chemicals', 'tosoh', 'tpp', 'trajan', 'transcat',
+  'karl storz', 'kementec', 'krishgen', 'kyokuto', 'labor diagnostika',
+  'larodan', 'leinco technologies', 'lgc', 'life technologies corporation',
+  'lkt', 'lkt labs', 'loewe', 'logos', 'lonza', 'lubio', 'lucigen', 'ludger',
+  'mabtech', 'macherey-nagel', 'mce', 'medac', 'medchemexpress', 'megazyme',
+  'mercedes scientific', 'merck', 'metrohm', 'mettler toledo', 'mikromol',
+  'millipore', 'minerva', 'mobitec', 'molecular probes', 'monosan', 'morphisto',
+  'mp biomedicals', 'mrc holland', 'mybiosource', 'mz', 'nalgene', 'neb',
+  'neofroxx', 'neolab', 'neubauer', 'nibsc', 'nimagen', 'nippon', 'nordic-mubio',
+  'novusbio', 'nunc', 'omega', 'orgentec', 'ortho', 'oxford nanopore technologies',
+  'oz biosciences', 'panreac', 'peprotech', 'perkin elmer', 'phadia', 'phytotech',
+  'pierce', 'pluriselect', 'polysciences', 'progen', 'promega', 'promocell',
+  'prosci-inc', 'prospec', 'proteinsimple', 'proteintech', 'qa bio', 'qed bioscience',
+  'qiagen', 'quidel', 'r-biopharm', 'r&d systems', 'radiometer', 'randox laboratories ltd.',
+  'raybiotech', 'rd_tocris', 'reagecon', 'reprocell', 'retsch', 'roche',
+  'roche diagnostics', 'rockland', 'rwd life science', 'sab', 'safc', 'santa crus',
+  'santa cruz', 'sapphire', 'sarstedt', 'sartorius', 'sbi', 'scharlab', 'scharlau',
+  'sciex', 'sebia', 'selleck', 'seracare', 'seramun', 'serva', 'shanghai korain biotech company',
+  'shodex', 'siemens', 'siemens healthineers', 'siemens allergy', 'siemens coag',
+  'sigma-aldrich', 'sigma laboratory centrifuges', 'signachem', 'simport', 'sinobiological',
+  'sisco', 'sitools biotech', 'smith & nephew', 'sony biotechnology', 'southernbiotech',
+  'spectrum', 'spherotech', 'stago', 'stemcell', 'stemcell', 'streck', 'stryker',
+  'supelco', 'synaptic systems', 'sysmex', 'sysmex uf', 'system biosciences',
+  'takara bio', 'targetmol', 'tci', 'ted pella', 'teknova', 'texwipe', 'tfs',
+  'the native antigen company', 'thermo fisher scientific', 'thermo', 'tocris bioscience',
+  'toku-e', 'toronto research chemicals', 'tosoh', 'tpp', 'trajan', 'transcat',
   'transgen', 'trc', 'trc-canada', 'united states biological', 'unknown',
   'upchurch', 'usbio', 'usbiological', 'usp', 'vazyme', 'vector laboratories',
   'vmd', 'vwr', 'wako', 'waters', 'welch', 'whatman', 'wilmad', 'worthington',
